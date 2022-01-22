988998b594baf2222de2318573fb7d4c048ceb21
Timothy Karani <kartimothy@gmail.com>
2022-01-22 13:42:51 +0300

    Can read query strings

--- snippetbox/main.go
+++ snippetbox/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
@@ -14,12 +16,12 @@ func home(w http.ResponseWriter, r *http.Request) {
 }
 
 func showSnippet(w http.ResponseWriter, r *http.Request) {
-	// w.Header().Set("Cache-Control", "public, max-age=2343242")
-	w.Header().Add("Cache-Control", "public")
-	w.Header().Add("Cache-Control", "max-age=234324")
-	// w.Header().Del("Cache-Control")
-	log.Println(w.Header().Get("Cache-Control"))
-	w.Write([]byte("Display a specific snippet..."))
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
 }
 
 func createSnippet(w http.ResponseWriter, r *http.Request) {
@@ -33,6 +35,11 @@ func createSnippet(w http.ResponseWriter, r *http.Request) {
 }
 
 func demoJson(w http.ResponseWriter, r *http.Request) {
+	// w.Header().Set("Cache-Control", "public, max-age=2343242")
+	w.Header().Add("Cache-Control", "public")
+	w.Header().Add("Cache-Control", "max-age=234324")
+	// w.Header().Del("Cache-Control")
+	log.Println(w.Header().Get("Cache-Control"))
 	w.Header().Set("Content-Type", "application/json")
 	// w.Header().Set("X-XSS-Protection", "1; mode=block")
 	w.Header()["X-XSS-Protection"] = []string{"1; mode=block"}
