9be80a468c6e5a06313f8466828518f64384675f
Timothy Karani <kartimothy@gmail.com>
2022-01-22 13:33:55 +0300

    Learnt how to work with headers

--- snippetbox/main.go
+++ snippetbox/main.go
@@ -14,6 +14,11 @@ func home(w http.ResponseWriter, r *http.Request) {
 }
 
 func showSnippet(w http.ResponseWriter, r *http.Request) {
+	// w.Header().Set("Cache-Control", "public, max-age=2343242")
+	w.Header().Add("Cache-Control", "public")
+	w.Header().Add("Cache-Control", "max-age=234324")
+	// w.Header().Del("Cache-Control")
+	log.Println(w.Header().Get("Cache-Control"))
 	w.Write([]byte("Display a specific snippet..."))
 }
 
@@ -27,11 +32,21 @@ func createSnippet(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Create a new snippet"))
 }
 
+func demoJson(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	// w.Header().Set("X-XSS-Protection", "1; mode=block")
+	w.Header()["X-XSS-Protection"] = []string{"1; mode=block"}
+	// w.Header().Del("Date") // Wont work
+	w.Header()["Date"] = nil
+	w.Write([]byte(`{"name":"Alex"}`))
+}
+
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet", showSnippet)
 	mux.HandleFunc("/snippet/create", createSnippet)
+	mux.HandleFunc("/json", demoJson)
 
 	log.Println("Starting server on :4000")
 	err := http.ListenAndServe(":4000", mux)
