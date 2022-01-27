95aff8bfa8ffe654a4eaf8bdc9396f47f877ae0f
Timothy Karani <kartimothy@gmail.com>
2022-01-27 22:28:34 +0300

    Can insert data

--- snippetbox/pkg/models/postgresql/snippets.go
+++ snippetbox/pkg/models/postgresql/snippets.go
@@ -2,6 +2,7 @@ package postgresql
 
 import (
 	"database/sql"
+	"fmt"
 
 	"github.com/c3n7/alex-edwards/snippetbox/pkg/models"
 )
@@ -10,8 +11,18 @@ type SnippetModel struct {
 	DB *sql.DB
 }
 
-func (m *SnippetModel) Insert(title, content, expires string) (int, error) {
-	return 0, nil
+func (m *SnippetModel) Insert(title, content string, expires int) (int, error) {
+
+	sttmt := fmt.Sprintf(`INSERT INTO snippets (title, content, created, expires) 
+	VALUES ($1, $2, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP + INTERVAL '%d days' ) RETURNING id;`, expires)
+
+	var insertedId int
+	err := m.DB.QueryRow(sttmt, title, content).Scan(&insertedId)
+	if err != nil {
+		return 0, err
+	}
+
+	return insertedId, nil
 }
 
 func (m *SnippetModel) Get(id int) (*models.Snippet, error) {
