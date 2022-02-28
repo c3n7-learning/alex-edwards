eb71d9b0d127eafb9a85eea8d031d67e1c770835
Timothy Karani <kartimothy@gmail.com>
2022-02-28 05:21:55 +0300

    Showing lists of snippets

--- snippetbox/cmd/web/templates.go
+++ snippetbox/cmd/web/templates.go
@@ -3,5 +3,6 @@ package main
 import "github.com/c3n7/alex-edwards/snippetbox/pkg/models"
 
 type templateData struct {
-	Snippet *models.Snippet
+	Snippet  *models.Snippet
+	Snippets []*models.Snippet
 }
