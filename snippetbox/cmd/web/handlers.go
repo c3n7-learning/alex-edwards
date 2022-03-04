15e66d55114b9c24c40e6a489f9be4ae6bbd3d92
Timothy Karani <kartimothy@gmail.com>
2022-03-04 19:52:49 +0300

    5.6. Custom template functions

--- snippetbox/cmd/web/handlers.go
+++ snippetbox/cmd/web/handlers.go
@@ -3,9 +3,7 @@ package main
 import (
 	"bytes"
 	"fmt"
-	"html/template"
 	"net/http"
-	"path/filepath"
 	"strconv"
 	"time"
 
@@ -19,38 +17,6 @@ func (app *application) addDefaultData(td *templateData, r *http.Request) *templ
 
 	td.CurrentYear = time.Now().Year()
 	return td
-}
-
-func newTemplateCache(dir string) (map[string]*template.Template, error) {
-	cache := map[string]*template.Template{}
-
-	pages, err := filepath.Glob(filepath.Join(dir, "*.page.tmpl"))
-	if err != nil {
-		return nil, err
-	}
-
-	for _, page := range pages {
-		name := filepath.Base(page)
-
-		ts, err := template.ParseFiles(page)
-		if err != nil {
-			return nil, err
-		}
-
-		ts, err = ts.ParseGlob(filepath.Join(dir, "*.layout.tmpl"))
-		if err != nil {
-			return nil, err
-		}
-
-		ts, err = ts.ParseGlob(filepath.Join(dir, "*.partial.tmpl"))
-		if err != nil {
-			return nil, err
-		}
-
-		cache[name] = ts
-	}
-
-	return cache, nil
 }
 
 func (app *application) render(w http.ResponseWriter, r *http.Request, name string, td *templateData) {
